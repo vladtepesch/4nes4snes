39b9a0fe6b26bd8697bf3e6dec538ad4aa5ef630
vlad <1601159+vladtepesch@users.noreply.github.com>
2019-10-23 21:30:48 +0200

    optimized report protocoll data (both axis in one byte) extracted reportdata into own file

---
+++ fournsnesHIDReportdescriptorData.c
@@ -0,0 +1,94 @@
+0x05,0x01,                //Usage_Page(Generic_Desktop_ID),
+0x09,0x04,                //Usage(Joystick_ID),
+0xa1,0x01,                //Collection(Clc_Application),
+0x85,0x01,                //    Report_ID (1)
+0x09,0x01,                //    Usage(Pointer_ID),
+0xa1,0x00,                //    Collection(Clc_Physical),
+0x09,0x30,                //      Usage(X_ID),
+0x09,0x31,                //      Usage(Y_ID),
+0x15,0x00,                //      Logical_Minimum (0)
+0x25,0x02,                //      Logical_Maximum (2)
+0x75,0x04,                //      Report_Size (4)
+0x95,0x02,                //      Report_Count (2)
+0x81,0x02,                //      Input(IOF_Variable),
+0xc0,                     //    End_Collection(),
+0x05,0x09,                //    Usage_Page(Button_ID),
+0x19,0x01,                //    Usage_Minimum(1),
+0x29,0x08,                //    Usage_Maximum(8),
+0x15,0x00,                //    Logical_Minimum(0),
+0x25,0x01,                //    Logical_Maximum(1),
+0x75,0x01,                //    Report_Size(1),
+0x95,0x08,                //    Report_Count(8),
+0x81,0x02,                //    Input(IOF_Variable),
+0xc0,                     //End_Collection(),
+0x05,0x01,                //Usage_Page(Generic_Desktop_ID),
+0x09,0x04,                //Usage(Joystick_ID),
+0xa1,0x01,                //Collection(Clc_Application),
+0x85,0x02,                //    Report_ID (2)
+0x09,0x01,                //    Usage(Pointer_ID),
+0xa1,0x00,                //    Collection(Clc_Physical),
+0x09,0x30,                //      Usage(X_ID),
+0x09,0x31,                //      Usage(Y_ID),
+0x15,0x00,                //      Logical_Minimum (0)
+0x25,0x02,                //      Logical_Maximum (2)
+0x75,0x04,                //      Report_Size (4)
+0x95,0x02,                //      Report_Count (2)
+0x81,0x02,                //      Input(IOF_Variable),
+0xc0,                     //    End_Collection(),
+0x05,0x09,                //    Usage_Page(Button_ID),
+0x19,0x01,                //    Usage_Minimum(1),
+0x29,0x08,                //    Usage_Maximum(8),
+0x15,0x00,                //    Logical_Minimum(0),
+0x25,0x01,                //    Logical_Maximum(1),
+0x75,0x01,                //    Report_Size(1),
+0x95,0x08,                //    Report_Count(8),
+0x81,0x02,                //    Input(IOF_Variable),
+0xc0,                     //End_Collection(),
+0x05,0x01,                //Usage_Page(Generic_Desktop_ID),
+0x09,0x04,                //Usage(Joystick_ID),
+0xa1,0x01,                //Collection(Clc_Application),
+0x85,0x03,                //    Report_ID (3)
+0x09,0x01,                //    Usage(Pointer_ID),
+0xa1,0x00,                //    Collection(Clc_Physical),
+0x09,0x30,                //      Usage(X_ID),
+0x09,0x31,                //      Usage(Y_ID),
+0x15,0x00,                //      Logical_Minimum (0)
+0x25,0x02,                //      Logical_Maximum (2)
+0x75,0x04,                //      Report_Size (4)
+0x95,0x02,                //      Report_Count (2)
+0x81,0x02,                //      Input(IOF_Variable),
+0xc0,                     //    End_Collection(),
+0x05,0x09,                //    Usage_Page(Button_ID),
+0x19,0x01,                //    Usage_Minimum(1),
+0x29,0x08,                //    Usage_Maximum(8),
+0x15,0x00,                //    Logical_Minimum(0),
+0x25,0x01,                //    Logical_Maximum(1),
+0x75,0x01,                //    Report_Size(1),
+0x95,0x08,                //    Report_Count(8),
+0x81,0x02,                //    Input(IOF_Variable),
+0xc0,                     //End_Collection(),
+0x05,0x01,                //Usage_Page(Generic_Desktop_ID),
+0x09,0x04,                //Usage(Joystick_ID),
+0xa1,0x01,                //Collection(Clc_Application),
+0x85,0x04,                //    Report_ID (4)
+0x09,0x01,                //    Usage(Pointer_ID),
+0xa1,0x00,                //    Collection(Clc_Physical),
+0x09,0x30,                //      Usage(X_ID),
+0x09,0x31,                //      Usage(Y_ID),
+0x15,0x00,                //      Logical_Minimum (0)
+0x25,0x02,                //      Logical_Maximum (2)
+0x75,0x04,                //      Report_Size (4)
+0x95,0x02,                //      Report_Count (2)
+0x81,0x02,                //      Input(IOF_Variable),
+0xc0,                     //    End_Collection(),
+0x05,0x09,                //    Usage_Page(Button_ID),
+0x19,0x01,                //    Usage_Minimum(1),
+0x29,0x08,                //    Usage_Maximum(8),
+0x15,0x00,                //    Logical_Minimum(0),
+0x25,0x01,                //    Logical_Maximum(1),
+0x75,0x01,                //    Report_Size(1),
+0x95,0x08,                //    Report_Count(8),
+0x81,0x02,                //    Input(IOF_Variable),
+0xc0,                     //End_Collection(),
+//Total:176 Bytes
+
